ca07594c4cdf25a3ae08577238d61504a7b68a5d
Michael Sprague <ms388@cornell.edu>
2019-06-02 13:56:17 -0400

    :fire: Remove unused code

--- src/js/modules/helpers.js
+++ src/js/modules/helpers.js
@@ -1,5 +1,3 @@
-import { register } from 'register-service-worker';
-
 export function canonical() {
   return `https://${window.location.hostname}/`;
 }
@@ -12,33 +10,6 @@ export function reloadWindow() {
   window.location.reload(true);
 }
 
-/* eslint-disable no-console */
-export function registerServiceWorker() {
-  register('/service-worker.js', {
-    // ready() {
-    //   console.log('Service worker is active.');
-    // },
-    // registered(registration) {
-    //   console.log('Service worker has been registered.', registration);
-    // },
-    // cached(registration) {
-    //   console.log('Content has been cached for offline use.', registration);
-    // },
-    // updatefound(registration) {
-    //   console.log('New content is downloading.', registration);
-    // },
-    updated() { // updated(registration)
-      console.log('New content has been downloaded.');
-    },
-    offline() {
-      console.info('No internet connection found. App is running in offline mode.');
-    },
-    error(error) {
-      console.error('Error during service worker registration:', error);
-    },
-  });
-}
-
 export function reportError(error) {
   const debug = process.env.NODE_ENV === 'development';
   if (debug) {
@@ -49,4 +20,3 @@ export function reportError(error) {
     /* eslint-enable no-undef */
   }
 }
-/* eslint-enable no-console */
